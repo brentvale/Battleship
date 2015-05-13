e499f971dc51e23884f345236e7cae106ba25248
Brent Vale <brentvale@gmail.com>
2015-05-13 08:57:45 -0700

    added bomb explosion sprite and missle launching sprite with looping code

--- public/javascripts/battleship_ui.js
+++ public/javascripts/battleship_ui.js
@@ -1,3 +1,9 @@
+var SPRITE_IMAGE_LENGTH = 64; 
+var SPRITE_IMAGE_ROWS = 8;
+var SPRITE_IMAGE_COLS = 8;
+var MS_FRAME = 10;
+var TOTAL_ANI_TIME = (SPRITE_IMAGE_ROWS * SPRITE_IMAGE_COLS) * MS_FRAME;
+
 function BattleshipUI($root, battleship){
   this.root = $root;
   this.battleship = battleship;
@@ -10,9 +16,76 @@ function BattleshipUI($root, battleship){
   this.shipCounter = 0;
   this.takenPositions = {};
   $("#pop").on("click", this.autopopulate.bind(this));
+  $("#practice-launch-missle").on("click", this.practiceLaunchMissle.bind(this));
+  $("#board2 .tile").on("click", this.launchMissle.bind(this));
+  $("#practice-explode-bomb").on("click", this.practiceExplosion.bind(this));
 }
 
 BattleshipUI.prototype = {
+  practiceLaunchMissle: function(){
+    var rowCount = 0;
+    var colCount = 0;
+
+    var animationInterval = window.setInterval(function() {
+      var position = (colCount*64*-1) + "px " + (rowCount*64*-1) + "px";
+      $(".missle").css("background-position", position);
+      console.log(position);
+      colCount += 1;
+      if(colCount >= SPRITE_IMAGE_COLS){
+        rowCount += 1;
+        colCount = 0;
+      }
+    }, MS_FRAME*3);
+    window.setTimeout(function() {
+      clearInterval(animationInterval);
+    },TOTAL_ANI_TIME*3);
+  },
+  launchMissle: function(event){
+    //getBoundingClientRect() => returns an elements absolute position on page
+    //get coordinates of bombedTile
+    var bombSpot = event.currentTarget.getBoundingClientRect();
+    //launchedMissle start coords are top: 445px; left: 10px; 
+    var launchedMissle = $(".traveling-missle");
+    var bombY = Math.floor(bombSpot.top) + 20;
+    var bombX = Math.floor(bombSpot.left) + 55;
+    
+    
+    //delta Y = 445 - bombY divided by total num of frames SPRITE_IMAGE_LENGTH
+    // var delY = (bombY - 430)/SPRITE_IMAGE_LENGTH;
+    var delY = 0;
+    //delta X = bombX - 10px divided by total num of frames SPRITE_IMAGE_LENGTH
+    // var delX = (bombX - 10)/SPRITE_IMAGE_LENGTH;
+    var delX = 3;
+    
+    var rowCount = 0;
+    var colCount = 0;
+    var aniCount = 0;
+    var animationInterval = window.setInterval(function() {
+      aniCount += 1;
+      if(aniCount % 6 === 0){
+        var position = (colCount*SPRITE_IMAGE_LENGTH*-1) + "px " + (rowCount*SPRITE_IMAGE_LENGTH*-1) + "px";
+        launchedMissle.css("background-position", position);
+        colCount += 1;
+        if(colCount >= SPRITE_IMAGE_COLS){
+          rowCount += 1;
+          colCount = 0;
+        }
+        console.log(position);
+      }
+      
+      var currentTop = launchedMissle.css("top");
+      var currentLeft = launchedMissle.css("left");
+      var newTop = parseInt(currentTop.slice(0,-2)) + (delY);
+      var newLeft = parseInt(currentLeft.slice(0,-2)) + (delX);
+      launchedMissle.css({"top": newTop, "left": newLeft});
+      
+    }, MS_FRAME);
+    var that = this;
+    window.setTimeout(function() {
+      clearInterval(animationInterval);
+      launchedMissle.css({"top": 430, "left": 10});
+    },TOTAL_ANI_TIME * 6); // multiple of animation count
+  },
   createGrids: function(){
     for (var i = 0; i < 2; i++) {
       for (var j = 0; j < 10; j++) {
@@ -159,7 +232,55 @@ BattleshipUI.prototype = {
   shoot: function(event){
     var oppCoord = [$(event.currentTarget).data("row"),
                         $(event.currentTarget).data("col") ];
-    alert("DESTROYED!: " + oppCoord);
+    //if the coords lead to a hit, explode bomb
+    //using same Interval as in 
+    var that = this;
+    window.setTimeout(function(){
+      that.loopExplosion(oppCoord)
+    },TOTAL_ANI_TIME * 6); // multiple of animation count
+    // alert("DESTROYED!: " + oppCoord);
+  },
+  loopExplosion: function(coords){
+    
+    var divToExplode = this.oppBoard[coords[0]][coords[1]];
+    $(divToExplode).addClass("explosion");
+    var rowCount = 0;
+    var colCount = 0;
+    
+    var explosionInterval = window.setInterval(function(){
+      //30 is the width of each image in the 120x120 16 grid sprite
+      var position = (colCount*30*-1) + "px " + (rowCount*30*-1) + "px";
+      $(divToExplode).css("background-position", position);
+      colCount += 1;
+      if(colCount >= 4){ //manual setting of col width: bomb image is 4 x 4
+        rowCount += 1;
+        colCount = 0;
+      }
+    }, 50);
+    
+    //time for clear interval is 16 times the above interval
+    window.setTimeout(function(){
+      clearInterval(explosionInterval)
+    }, 800);
+  },
+  practiceExplosion: function(event){
+    var divToExplode = $(".practice-bomb");
+    var rowCount = 0;
+    var colCount = 0;
+    
+    var explosionInterval = window.setInterval(function(){
+      
+      var position = (colCount*64*-1) + "px " + (rowCount*64*-1) + "px";
+      $(divToExplode).css("background-position", position);
+      colCount += 1;
+      if(colCount >= 4){ //manual setting of col width: bomb image is 4 x 4
+        rowCount += 1;
+        colCount = 0;
+      }
+    }, 50);
+    
+    //time for clear interval is 16 times the above interval
+    window.setTimeout(function(){clearInterval(explosionInterval)}, 800);
   },
   autopopulate: function(){
     var ships = this.battleship.shipsToPlace;
