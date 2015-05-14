991cde1c1a140d08ae34650656c03496a5b9ced4
Brent Vale <brentvale@gmail.com>
2015-05-14 14:15:32 -0700

    game server started, moving client logic to battleship and battleshipUI

--- public/javascripts/battleship.js
+++ public/javascripts/battleship.js
@@ -1,27 +1,33 @@
 (function (root) {
   var App = root.App = (root.App || {});
   var Battleship = App.Battleship = function(socket){
+    this.socket = socket;
+    this.shipsToPlace = this.createShips();
+    this.registerHandlers();
+  }
+
+  Battleship.prototype = {
+    createShips: function(){
+      var ships = [];
+      var shipOne = new Ship({length:2});
+      ships.push(shipOne);
+      var shipTwo = new Ship({length:3});
+      ships.push(shipTwo);
+
+      var shipThree = new Ship({length:3});
+      ships.push(shipThree);
+
+      var shipFour = new Ship({length:4});
+      ships.push(shipFour);
+
+      var shipFive = new Ship({length:5});
+      ships.push(shipFive);
+      return ships;
+    },
+    registerHandlers: function() {
+      var 
+    }
     
-  this.shipsToPlace = this.createShips();
-}
-
-Battleship.prototype = {
-  createShips: function(){
-    var ships = [];
-    var shipOne = new Ship({length:2});
-    ships.push(shipOne);
-    var shipTwo = new Ship({length:3});
-    ships.push(shipTwo);
-
-    var shipThree = new Ship({length:3});
-    ships.push(shipThree);
-
-    var shipFour = new Ship({length:4});
-    ships.push(shipFour);
-
-    var shipFive = new Ship({length:5});
-    ships.push(shipFive);
-    return ships;
   }
   
 }(this));
--- public/javascripts/game_states.js
+++ public/javascripts/game_states.js
@@ -1,6 +1,12 @@
-var PLACE_SHIPS = true;
-var SHOOT = false;
-function GameStates(){
-  this.PLACE_SHIPS = PLACE_SHIPS;
-  this.SHOOT = SHOOT;
+// var PLACE_SHIPS = true;
+// var SHOOT = false;
+// function GameStates(){
+//   this.PLACE_SHIPS = PLACE_SHIPS;
+//   this.SHOOT = SHOOT;
+// }
+
+
+var GAME_STATES = {
+  PLACE_SHIPS = "PLACE_SHIPS",
+  SHOOT = "SHOOT"
 }
